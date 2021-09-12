7c92b05c82571dfbff8dcca833de6e4be6d5a613
nhan2804 <akainu345@gmail.com>
2021-09-12 15:44:49 +0700

    set build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,48 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import reactRefresh from "@vitejs/plugin-react-refresh";
+// import { resolve } from "path";
+// import eslint from "@rbnlffl/rollup-plugin-eslint";
+// // import eslintPlugin from "vite-plugin-eslint";
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     reactRefresh(),
+//     eslint({
+//       extensions: ["js", "jsx", "ts", "tsx"],
+//       filterInclude: ["**/*.{js,ts,jsx,tsx}"],
+//       throwOnError: true,
+//     }),
+//   ],
+//   // define: {
+//   //   global: "window",
+//   // },
+//   resolve: {
+//     alias: {
+//       "@pages": resolve(__dirname, "./src/pages"),
+//       "@modules": resolve(__dirname, "./src/modules"),
+//       "@routes": resolve(__dirname, "./src/routes"),
+//       "@app": resolve(__dirname, "./src/app"),
+
+//       "@hooks": resolve(__dirname, "./src/common/hooks"),
+//       "@components": resolve(__dirname, "./src/common/components"),
+//       "@action": resolve(__dirname, "./src/redux/action"),
+//       "@config": resolve(__dirname, "./src/config"),
+//       "@helper": resolve(__dirname, "./src/helper"),
+//       "@assets": resolve(__dirname, "./src/assets"),
+//       "@data": resolve(__dirname, "./src/data"),
+//       "@redux": resolve(__dirname, "./src/redux"),
+//       "@icon": resolve(__dirname, "./src/common/components/icons"),
+//     },
+//   },
+// });
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import { resolve } from "path";
 import eslint from "@rbnlffl/rollup-plugin-eslint";
-// import eslintPlugin from "vite-plugin-eslint";
+import { resolve } from "path";
 // https://vitejs.dev/config/
-export default defineConfig({
+/**
+ * @type {import('vite').UserConfig}
+ */
+const config = ({ command }) => ({
   plugins: [
     reactRefresh(),
     eslint({
@@ -13,6 +51,10 @@ export default defineConfig({
       throwOnError: true,
     }),
   ],
+  base: command === "serve" ? "" : "/dist/",
+  build: {
+    manifest: true,
+  },
   // define: {
   //   global: "window",
   // },
@@ -22,7 +64,6 @@ export default defineConfig({
       "@modules": resolve(__dirname, "./src/modules"),
       "@routes": resolve(__dirname, "./src/routes"),
       "@app": resolve(__dirname, "./src/app"),
-
       "@hooks": resolve(__dirname, "./src/common/hooks"),
       "@components": resolve(__dirname, "./src/common/components"),
       "@action": resolve(__dirname, "./src/redux/action"),
@@ -35,3 +76,4 @@ export default defineConfig({
     },
   },
 });
+export default config;
